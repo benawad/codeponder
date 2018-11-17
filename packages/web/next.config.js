f2561b3513ae65315e74a5addb0047e4c6fc95cd
Ben Awad <benawad97@gmail.com>
2018-11-17 12:40:22 -0600

    semantic ui works

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,2 +1,24 @@
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+const withTypescript = require("@zeit/next-typescript");
+const withCss = require("@zeit/next-css");
+
+const wcss = withCss({
+  webpack: config => {
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/i,
+      use: {
+        loader: "url-loader",
+        options: {
+          limit: 8192,
+          publicPath: "./",
+          outputPath: "static/css/",
+          name: "[name].[ext]"
+        }
+      }
+    });
+    return config;
+  }
+});
+
+module.exports = withTypescript({
+  ...wcss
+});
--- packages/web/pages/register.tsx
+++ packages/web/pages/register.tsx
@@ -1,7 +1,18 @@
-import Layout from "../components/Layout";
+import { Button, Checkbox, Form } from "semantic-ui-react";
 
 export default () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <div>register</div>
-  </Layout>
+  <Form>
+    <Form.Field>
+      <label>First Name</label>
+      <input placeholder="First Name" />
+    </Form.Field>
+    <Form.Field>
+      <label>Last Name</label>
+      <input placeholder="Last Name" />
+    </Form.Field>
+    <Form.Field>
+      <Checkbox label="I agree to the Terms and Conditions" />
+    </Form.Field>
+    <Button type="submit">Submit</Button>
+  </Form>
 );
