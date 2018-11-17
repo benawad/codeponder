2c7217e5d44b491bfddf333da9ae493b83db5651
Ben Awad <benawad97@gmail.com>
2018-11-17 12:10:20 -0600

    login

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,19 +1,58 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import * as express from "express";
+import * as session from "express-session";
+import * as connectRedis from "connect-redis";
+import * as cors from "cors";
+
 import { createTypeormConn } from "./createTypeormConn";
 import { createSchema } from "./createSchema";
+import { redis } from "./redis";
+
+// @todo move to .env
+const SESSION_SECRET = "ajslkjalksjdfkl";
+const RedisStore = connectRedis(session);
+
+const corsOptions = {
+  credentials: true,
+  origin: "http://localhost:3000"
+};
 
 const startServer = async () => {
   await createTypeormConn();
 
   const app = express();
 
+  app.use(cors(corsOptions));
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis as any
+      }),
+      name: "qid",
+      secret: SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 365 * 7 // 7 days
+      }
+    })
+  );
+
   const server = new ApolloServer({
-    schema: createSchema()
+    schema: createSchema(),
+    context: ({ req }: any) => ({
+      req
+    })
   });
 
-  server.applyMiddleware({ app }); // app is from an existing express app
+  server.applyMiddleware({
+    app,
+    cors: false
+  });
 
   app.listen({ port: 4000 }, () =>
     console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
--- packages/server/src/types.ts
+++ packages/server/src/types.ts
@@ -1,3 +1,9 @@
+export interface LoginInput {
+  usernameOrEmail: string;
+
+  password: string;
+}
+
 export interface RegisterInput {
   username: string;
 
@@ -5,46 +11,9 @@ export interface RegisterInput {
 
   password: string;
 }
-
-// ====================================================
-// Types
-// ====================================================
-
-export interface Query {
-  me?: User | null;
-}
-
-export interface User {
-  id: string;
-
-  username: string;
-
-  email: string;
-}
-
-export interface Mutation {
-  register: RegisterResponse;
-}
-
-export interface RegisterResponse {
-  errors?: Error[] | null;
-}
-
-export interface Error {
-  path: string;
-
-  message: string;
-}
-
-// ====================================================
-// Arguments
-// ====================================================
-
-export interface RegisterMutationArgs {
-  input: RegisterInput;
-}
-
 import { GraphQLResolveInfo } from "graphql";
+
+import { MyContext } from "./context";
 
 export type Resolver<Result, Parent = {}, Context = {}, Args = {}> = (
   parent: Parent,
@@ -80,19 +49,19 @@ export type SubscriptionResolver<
   | ISubscriptionResolverObject<Result, Parent, Context, Args>;
 
 export namespace QueryResolvers {
-  export interface Resolvers<Context = {}, TypeParent = {}> {
+  export interface Resolvers<Context = MyContext, TypeParent = {}> {
     me?: MeResolver<User | null, TypeParent, Context>;
   }
 
-  export type MeResolver<R = User | null, Parent = {}, Context = {}> = Resolver<
-    R,
-    Parent,
-    Context
-  >;
+  export type MeResolver<
+    R = User | null,
+    Parent = {},
+    Context = MyContext
+  > = Resolver<R, Parent, Context>;
 }
 
 export namespace UserResolvers {
-  export interface Resolvers<Context = {}, TypeParent = User> {
+  export interface Resolvers<Context = MyContext, TypeParent = User> {
     id?: IdResolver<string, TypeParent, Context>;
 
     username?: UsernameResolver<string, TypeParent, Context>;
@@ -100,65 +69,147 @@ export namespace UserResolvers {
     email?: EmailResolver<string, TypeParent, Context>;
   }
 
-  export type IdResolver<R = string, Parent = User, Context = {}> = Resolver<
-    R,
-    Parent,
-    Context
-  >;
+  export type IdResolver<
+    R = string,
+    Parent = User,
+    Context = MyContext
+  > = Resolver<R, Parent, Context>;
   export type UsernameResolver<
     R = string,
     Parent = User,
-    Context = {}
+    Context = MyContext
   > = Resolver<R, Parent, Context>;
-  export type EmailResolver<R = string, Parent = User, Context = {}> = Resolver<
-    R,
-    Parent,
-    Context
-  >;
+  export type EmailResolver<
+    R = string,
+    Parent = User,
+    Context = MyContext
+  > = Resolver<R, Parent, Context>;
 }
 
 export namespace MutationResolvers {
-  export interface Resolvers<Context = {}, TypeParent = {}> {
+  export interface Resolvers<Context = MyContext, TypeParent = {}> {
+    login?: LoginResolver<LoginResponse, TypeParent, Context>;
+
     register?: RegisterResolver<RegisterResponse, TypeParent, Context>;
+  }
+
+  export type LoginResolver<
+    R = LoginResponse,
+    Parent = {},
+    Context = MyContext
+  > = Resolver<R, Parent, Context, LoginArgs>;
+  export interface LoginArgs {
+    input: LoginInput;
   }
 
   export type RegisterResolver<
     R = RegisterResponse,
     Parent = {},
-    Context = {}
+    Context = MyContext
   > = Resolver<R, Parent, Context, RegisterArgs>;
   export interface RegisterArgs {
     input: RegisterInput;
   }
 }
 
+export namespace LoginResponseResolvers {
+  export interface Resolvers<Context = MyContext, TypeParent = LoginResponse> {
+    errors?: ErrorsResolver<Error[] | null, TypeParent, Context>;
+
+    user?: UserResolver<User | null, TypeParent, Context>;
+  }
+
+  export type ErrorsResolver<
+    R = Error[] | null,
+    Parent = LoginResponse,
+    Context = MyContext
+  > = Resolver<R, Parent, Context>;
+  export type UserResolver<
+    R = User | null,
+    Parent = LoginResponse,
+    Context = MyContext
+  > = Resolver<R, Parent, Context>;
+}
+
+export namespace ErrorResolvers {
+  export interface Resolvers<Context = MyContext, TypeParent = Error> {
+    path?: PathResolver<string, TypeParent, Context>;
+
+    message?: MessageResolver<string, TypeParent, Context>;
+  }
+
+  export type PathResolver<
+    R = string,
+    Parent = Error,
+    Context = MyContext
+  > = Resolver<R, Parent, Context>;
+  export type MessageResolver<
+    R = string,
+    Parent = Error,
+    Context = MyContext
+  > = Resolver<R, Parent, Context>;
+}
+
 export namespace RegisterResponseResolvers {
-  export interface Resolvers<Context = {}, TypeParent = RegisterResponse> {
+  export interface Resolvers<
+    Context = MyContext,
+    TypeParent = RegisterResponse
+  > {
     errors?: ErrorsResolver<Error[] | null, TypeParent, Context>;
   }
 
   export type ErrorsResolver<
     R = Error[] | null,
     Parent = RegisterResponse,
-    Context = {}
+    Context = MyContext
   > = Resolver<R, Parent, Context>;
 }
 
-export namespace ErrorResolvers {
-  export interface Resolvers<Context = {}, TypeParent = Error> {
-    path?: PathResolver<string, TypeParent, Context>;
+// ====================================================
+// Types
+// ====================================================
 
-    message?: MessageResolver<string, TypeParent, Context>;
-  }
+export interface Query {
+  me?: User | null;
+}
 
-  export type PathResolver<R = string, Parent = Error, Context = {}> = Resolver<
-    R,
-    Parent,
-    Context
-  >;
-  export type MessageResolver<
-    R = string,
-    Parent = Error,
-    Context = {}
-  > = Resolver<R, Parent, Context>;
+export interface User {
+  id: string;
+
+  username: string;
+
+  email: string;
+}
+
+export interface Mutation {
+  login: LoginResponse;
+
+  register: RegisterResponse;
+}
+
+export interface LoginResponse {
+  errors?: Error[] | null;
+
+  user?: User | null;
+}
+
+export interface Error {
+  path: string;
+
+  message: string;
+}
+
+export interface RegisterResponse {
+  errors?: Error[] | null;
+}
+
+// ====================================================
+// Arguments
+// ====================================================
+
+export interface LoginMutationArgs {
+  input: LoginInput;
+}
+export interface RegisterMutationArgs {
+  input: RegisterInput;
 }
