9257937fa169d8cb2ee036d94b08ae9aee8072d1
Ben Awad <benawad97@gmail.com>
2018-11-18 01:36:26 -0600

    remove composite on server

--- packages/server/src/modules/user/register/resolvers.ts
+++ packages/server/src/modules/user/register/resolvers.ts
@@ -1,9 +1,29 @@
 import * as argon from "argon2";
-import { registerSchema } from "@codeponder/common";
+import * as yup from "yup";
 
 import { MutationResolvers } from "../../../types";
 import { User } from "../../../entity/User";
 import { formatYupError } from "../../../utils/formatYupErrors";
+
+export const registerSchema = yup.object().shape({
+  username: yup
+    .string()
+    .matches(/^[a-zA-Z0-9]*$/, "username can only contain letters and numbers")
+    .min(3)
+    .max(30)
+    .required(),
+  email: yup
+    .string()
+    .email()
+    .min(3)
+    .max(500)
+    .required(),
+  password: yup
+    .string()
+    .min(5)
+    .max(1000)
+    .required()
+});
 
 export const resolvers: MutationResolvers.Resolvers = {
   register: async (_, { input }) => {
