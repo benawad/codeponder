10b797c36ef9dfd4ceb732d0fc37f4a2cef23b96
Ben Awad <benawad97@gmail.com>
2018-11-18 00:27:35 -0600

    add wait for create typeorm conn

--- packages/server/src/createTypeormConn.ts
+++ packages/server/src/createTypeormConn.ts
@@ -34,7 +34,20 @@ const prod = {
 };
 
 export const createTypeormConn = async () => {
-  return createConnection(
-    process.env.NODE_ENV === "production" ? (prod as any) : (dev as any)
-  );
+  let retries = 5;
+  while (retries) {
+    try {
+      return createConnection(
+        process.env.NODE_ENV === "production" ? (prod as any) : (dev as any)
+      );
+    } catch (err) {
+      console.log(err);
+      retries -= 1;
+      console.log(`retries left: ${retries}`);
+      // wait 5 seconds
+      await new Promise(res => setTimeout(res, 5000));
+    }
+  }
+
+  return null;
 };
