2615b5ea94925b887f9a55151c767f518a58cb1e
Ben Awad <benawad97@gmail.com>
2018-11-17 23:16:57 -0600

    landing page

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,9 +1,31 @@
-import Link from 'next/link'
-import Layout from '../components/Layout';
+import Link from "next/link";
+import Layout from "../components/Layout";
+import { Button } from "semantic-ui-react";
 
 export default () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p><Link href='/about'><a>About</a></Link></p>
+  <Layout title="Landing Page | Code Ponder">
+    <h1>Code Ponder 🤔</h1>
+    <h4>Marketplace for Code Reviews</h4>
+    <h2>How it works</h2>
+    <ol>
+      <li>Coder requests a code review</li>
+      <li>Experts offer assistance</li>
+      <li>Coder picks favorite expert</li>
+      <li>Selected expert receives a request to review the code on github</li>
+    </ol>
+    <p>
+      <Link href="/register">
+        <a>
+          <Button>Register as a Coder</Button>
+        </a>
+      </Link>
+    </p>
+    <p>
+      <Link href="/register">
+        <a>
+          <Button>Register as a Expert</Button>
+        </a>
+      </Link>
+    </p>
   </Layout>
-)
+);
