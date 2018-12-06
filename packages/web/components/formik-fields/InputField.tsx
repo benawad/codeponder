0d7bc9e0dc5c2f648205450b4f2d290cf966341f
Ben Awad <benawad97@gmail.com>
2018-12-05 19:38:35 -0600

    start on pick username form

---
+++ packages/web/components/formik-fields/InputField.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { Input } from "@codeponder/ui";
+import { FieldProps } from "formik";
+
+export const InputField = ({
+  field, // { name, value, onChange, onBlur }
+  form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  ...props
+}: FieldProps<any>) => {
+  const errorText = touched[field.name] && errors[field.name];
+
+  return <Input errorText={errorText} {...field} {...props} />;
+};
