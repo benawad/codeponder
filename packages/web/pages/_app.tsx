b6688530e500a06e4cfe02dadd5cd659868c005d
Jonah Meijers <jonahmeijers97@gmail.com>
2018-12-07 03:14:57 +0100

    exported theme from ui package and imported it into web package

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -3,15 +3,18 @@ import React from "react";
 import { ApolloProvider } from "react-apollo";
 
 import withApolloClient from "../lib/with-apollo-client";
+import { ThemeProvider, theme } from "@codeponder/ui";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props as any;
     return (
       <Container>
-        <ApolloProvider client={apolloClient}>
-          <Component {...pageProps} />
-        </ApolloProvider>
+        <ThemeProvider theme={theme}>
+          <ApolloProvider client={apolloClient}>
+            <Component {...pageProps} />
+          </ApolloProvider>
+        </ThemeProvider>
       </Container>
     );
   }
