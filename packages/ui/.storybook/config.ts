c26443f1d9f926460d70410052a1538b571e3759
Jonah Meijers <jonahmeijers97@gmail.com>
2018-12-07 03:12:12 +0100

    added themes provider to storybook

--- packages/ui/.storybook/config.ts
+++ packages/ui/.storybook/config.ts
@@ -1,10 +1,13 @@
-import { configure } from "@storybook/react";
-
+import { configure, addDecorator } from "@storybook/react";
+import theme from "../src/theme";
+import { withThemesProvider } from "storybook-addon-styled-component-theme";
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /.story.tsx$/);
 
 function loadStories() {
   req.keys().forEach(req);
 }
+
+addDecorator(withThemesProvider([theme]));
 
 configure(loadStories, module);
