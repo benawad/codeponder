6e71c917a0be4f168770436130835f4c6cce0fb9
Ben Awad <benawad97@gmail.com>
2018-12-10 19:33:08 -0600

    find code review questions

--- packages/server/src/modules/code-review-question/CodeReviewQuestionResolver.ts
+++ packages/server/src/modules/code-review-question/CodeReviewQuestionResolver.ts
@@ -1,15 +1,16 @@
-import { Resolver, Ctx, Mutation, Arg, Authorized } from "type-graphql";
+import { Resolver, Ctx, Mutation, Arg, Authorized, Query } from "type-graphql";
 import { MyContext } from "../../types/Context";
 import { CodeReviewQuestion } from "../../entity/CodeReviewQuestion";
 import { CreateCodeReviewQuestionInput } from "./shared/CreateCodeReviewQuestionInput";
 import { CreateCodeReviewResponse } from "./shared/CreateCodeReviewResponse";
+import { FindConditions } from "typeorm";
 
 @Resolver(CodeReviewQuestion)
 export class CodeReviewQuestionResolver {
   constructor() {}
 
   @Authorized()
-  @Mutation(() => CreateCodeReviewResponse, { nullable: true })
+  @Mutation(() => CreateCodeReviewResponse)
   async createCodeReviewQuestion(
     @Arg("question") question: CreateCodeReviewQuestionInput,
     @Ctx()
@@ -22,5 +23,27 @@ export class CodeReviewQuestionResolver {
     return {
       codeReviewQuestion
     };
+  }
+
+  @Query(() => [CodeReviewQuestion])
+  async findCodeReviewQuestions(
+    @Arg("path", { nullable: true }) path: string,
+    @Arg("repo") repo: string,
+    @Arg("branch") branch: string,
+    @Arg("username") username: string
+  ) {
+    const where: FindConditions<CodeReviewQuestion> = {
+      repo,
+      branch,
+      username
+    };
+
+    if (path) {
+      where.path = path;
+    }
+
+    return CodeReviewQuestion.find({
+      where
+    });
   }
 }
