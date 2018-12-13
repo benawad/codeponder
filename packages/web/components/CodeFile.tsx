dc9eefe2d78675fd130b2ef5230bdade33f74a3d
audiBookning <audiobookning@gmail.com>
2018-12-13 04:31:26 +0000

    prism implementation using babel-plugin-prismjs

--- packages/web/components/CodeFile.tsx
+++ packages/web/components/CodeFile.tsx
@@ -1,17 +1,23 @@
-import Highlight, { defaultProps } from "prism-react-renderer";
-import * as Prism from "prismjs";
-import "prismjs/themes/prism.css";
-import "prismjs/themes/prism-solarizedlight.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.css";
-import "prismjs/plugins/line-numbers/prism-line-numbers.js";
+//import Highlight, { defaultProps } from 'prism-react-renderer';
+import * as Prism from 'prismjs';
+
+//********************************* */
+// to import a specific language
+//import 'prismjs/components/prism-typescript';
+//********************************* */
+
+//import 'prismjs/themes/prism.css';
+//import 'prismjs/themes/prism-solarizedlight.css';
+//import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+//import 'prismjs/plugins/line-numbers/prism-line-numbers.js';
 
 import {
   CreateCodeReviewQuestionComponent,
   FindCodeReviewQuestionsComponent
-} from "./apollo-components";
-import { QuestionReply } from "./QuestionReply";
-import { useInputValue } from "../utils/useInputValue";
-import { useEffect } from "react";
+} from './apollo-components';
+import { QuestionReply } from './QuestionReply';
+import { useInputValue } from '../utils/useInputValue';
+import { useEffect } from 'react';
 
 interface Props {
   code: string | null;
@@ -28,33 +34,36 @@ export const CodeFile: React.SFC<Props> = ({
   username,
   repo
 }) => {
-  const [startingLineNum, startingLineNumChange] = useInputValue("0");
-  const [endingLineNum, endingLineNumChange] = useInputValue("0");
-  const [text, textChange] = useInputValue("");
+  const [startingLineNum, startingLineNumChange] = useInputValue('0');
+  const [endingLineNum, endingLineNumChange] = useInputValue('0');
+  const [text, textChange] = useInputValue('');
 
   useEffect(() => {
-    // Prism.highlightAll();
+    Prism.highlightAll();
     return () => {};
-  });
+  }, []);
 
-  const extension = path ? path.split(".").pop() : "";
+  const extension = path ? path.split('.').pop() : '';
 
-  console.log(defaultProps);
+  //console.log(defaultProps);
 
   return (
     <CreateCodeReviewQuestionComponent>
       {mutate => (
         <>
+          <pre className={`line-numbers language-typescript`}>
+            <code>{code}</code>
+          </pre>
           {/* <pre className={`line-numbers`}>
             <code className={`language-${extension}`}>
-              {(code || "").split("\n").map((token, i) => (
+              {(code || '').split('\n').map((token, i) => (
                 <div key={i} className="token-line">
                   {token}
                 </div>
               ))}
             </code>
           </pre> */}
-          <Highlight
+          {/* <Highlight
             {...defaultProps}
             theme={undefined}
             code={code}
@@ -84,7 +93,7 @@ export const CodeFile: React.SFC<Props> = ({
                 ))}
               </pre>
             )}
-          </Highlight>
+          </Highlight> */}
           <form
             onSubmit={async e => {
               e.preventDefault();
@@ -145,7 +154,7 @@ export const CodeFile: React.SFC<Props> = ({
                       <div>|{crq.creator.username}|</div>
                       <div>{crq.text}</div>
                       {crq.replies.map(reply => (
-                        <div key={reply.id} style={{ color: "pink" }}>
+                        <div key={reply.id} style={{ color: 'pink' }}>
                           <div>${reply.creator.username}$</div>
                           {reply.text}
                         </div>
