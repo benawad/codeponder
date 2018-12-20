815dbb9c31841cef9d90d8fefaaffbd1265de4df
Ben Awad <benawad97@gmail.com>
2018-12-19 18:56:46 -0600

    include code snippet

--- packages/web/components/CodeFile.tsx
+++ packages/web/components/CodeFile.tsx
@@ -69,11 +69,17 @@ export const CodeFile: React.SFC<Props> = ({ code, path, postId }) => {
                 <form
                   onSubmit={async e => {
                     e.preventDefault();
+                    const start = parseInt(startingLineNum, 10);
+                    const end = parseInt(endingLineNum, 10);
                     const response = await mutate({
                       variables: {
                         codeReviewQuestion: {
-                          startingLineNum: parseInt(startingLineNum, 10),
-                          endingLineNum: parseInt(endingLineNum, 10),
+                          startingLineNum: start,
+                          endingLineNum: end,
+                          codeSnippet: (code || "")
+                            .split("\n")
+                            .slice(start - 1, end)
+                            .join("\n"),
                           text: text,
                           path,
                           postId,
