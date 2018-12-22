c3fed7f2133d3d45d884f4b2e5c476d77ffc016c
paean99 <audiobookning@gmail.com>
2018-12-22 21:23:34 +0000

    Added highlight using Styled Components

--- packages/web/components/CodeFile.tsx
+++ packages/web/components/CodeFile.tsx
@@ -2,11 +2,14 @@
 //import "prismjs/themes/prism.css";
 import "prismjs/themes/prism-coy.css";
 
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Highlight, { defaultProps, Language } from "prism-react-renderer";
 //import theme from "prism-react-renderer/themes/vsDarkPlus";
 
-import { FindCodeReviewQuestionsComponent } from "./apollo-components";
+import {
+  FindCodeReviewQuestionsComponent,
+  FindCodeReviewQuestionsQuery,
+} from "./apollo-components";
 import { filenameToLang } from "../utils/filenameToLang";
 import { QuestionSection } from "./QuestionSection";
 
@@ -16,27 +19,21 @@ interface Props {
   postId: string;
 }
 
-const Pre = styled.pre`
-  text-align: left;
-  margin: 4em 0;
-  padding: 0.5em;
+const SelectLines = (prop: FindCodeReviewQuestionsQuery) => {
+  const styles = prop.findCodeReviewQuestions.reduce((total, current) => {
+    return (total += `
+     & .token-line:nth-child(n+${current.startingLineNum}):nth-child(-n+${
+      current.endingLineNum
+    }) {
+      background-color: #ffffcc;
+    }
+     `);
+  }, "");
 
-  & .token-line {
-    line-height: 1.3em;
-    height: 1.3em;
-  }
-
-  & .token-line:nth-child(odd) {
-    background: #f3faff;
-  }
-`;
-
-const LineNo = styled.span`
-  display: inline-block;
-  width: 2em;
-  user-select: none;
-  opacity: 0.3;
-`;
+  return css`
+    ${styles}
+  `;
+};
 
 export const CodeFile: React.SFC<Props> = ({ code, path, postId }) => {
   const lang: Language = path ? filenameToLang(path) : "";
@@ -52,9 +49,29 @@ export const CodeFile: React.SFC<Props> = ({ code, path, postId }) => {
           return null;
         }
 
-        const dataLines = data.findCodeReviewQuestions.map(q => {
-          return `${q.startingLineNum}-${q.endingLineNum}`;
-        });
+        const Pre = styled.pre`
+          text-align: left;
+          margin: 4em 0;
+          padding: 0.5em;
+
+          & .token-line {
+            line-height: 1.3em;
+            height: 1.3em;
+          }
+
+          & .token-line:nth-child(odd) {
+            background: #f3faff;
+          }
+
+          ${SelectLines(data)};
+        `;
+
+        const LineNo = styled.span`
+          display: inline-block;
+          width: 2em;
+          user-select: none;
+          opacity: 0.3;
+        `;
 
         return (
           <>
