c18f1eb6b265969f8df637f6fa968cbf0750ee1a
Luiz Felipe <luiz.lffg@gmail.com>
2019-01-25 17:03:41 -0200

    refactor(userLoader): Use Promise.all() in the loader

--- packages/server/src/loaders/userLoader.ts
+++ packages/server/src/loaders/userLoader.ts
@@ -3,14 +3,9 @@ import { User } from "../entity/User";
 
 export const userLoader = () =>
   new DataLoader(async (keys: string[]) => {
-    const users = await User.findByIds(keys);
-
-    const userMap: { [key: string]: User } = {};
-
-    users.forEach(u => {
-      userMap[u.id] = u;
-    });
-
-    // O(n) * O(1)
-    return keys.map(k => userMap[k]);
+    return Promise.all(
+      keys.map(id => {
+        return User.find({ where: { id } });
+      })
+    );
   });
