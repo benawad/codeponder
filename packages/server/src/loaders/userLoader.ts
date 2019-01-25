8e771fb0534e0754894d7f4193fa44ac64b2b93c
Luiz Felipe <luiz.lffg@gmail.com>
2019-01-25 17:22:43 -0200

    fix(userLoader): Remove async function

--- packages/server/src/loaders/userLoader.ts
+++ packages/server/src/loaders/userLoader.ts
@@ -2,7 +2,7 @@ import * as DataLoader from "dataloader";
 import { User } from "../entity/User";
 
 export const userLoader = () =>
-  new DataLoader(async (keys: string[]) => {
+  new DataLoader((keys: string[]) => {
     return Promise.all(
       keys.map(id => {
         return User.find({ where: { id } });
