2d622f882db939803af1f69b4f8e99fa7183106c
Ben Awad <benawad97@gmail.com>
2019-02-17 16:03:52 -0600

    create notif on mention

--- packages/server/src/modules/comment/resolver.ts
+++ packages/server/src/modules/comment/resolver.ts
@@ -1,9 +1,15 @@
+import rehypeStringify from "rehype-stringify";
+import remarkParse from "remark-parse";
+import remarkPing from "remark-ping";
+import remark2rehype from "remark-rehype";
 import { Arg, Ctx, Mutation, Resolver, UseMiddleware } from "type-graphql";
-import { Repository } from "typeorm";
+import { In, Repository } from "typeorm";
 import { InjectRepository } from "typeorm-typedi-extensions";
+import * as unified from "unified";
 import { Comment } from "../../entity/Comment";
 import { Question } from "../../entity/Question";
 import { QuestionCommentNotification } from "../../entity/QuestionCommentNotification";
+import { User } from "../../entity/User";
 import { MyContext } from "../../types/Context";
 import { isAuthenticated } from "../shared/middleware/isAuthenticated";
 import { CreateCommentInput } from "./createInput";
@@ -14,6 +20,8 @@ export class CommentResolver {
   constructor(
     @InjectRepository(Comment)
     private readonly commentRepo: Repository<Comment>,
+    @InjectRepository(User)
+    private readonly userRepo: Repository<User>,
     @InjectRepository(Question)
     private readonly questionRepo: Repository<Question>,
     @InjectRepository(QuestionCommentNotification)
@@ -35,12 +43,45 @@ export class CommentResolver {
 
     const question = await this.questionRepo.findOne(input.questionId);
 
-    await this.questionCommentNotificationRepo.save({
+    // no await will run it in the background??
+    this.questionCommentNotificationRepo.save({
       commentId: comment.id,
       questionId: input.questionId,
       userToNotifyId: question!.creatorId,
       type: "reply",
     });
+
+    unified()
+      .use(remarkParse)
+      .use(remarkPing, { pingUsername: true, userURL: () => "" })
+      .use(remark2rehype)
+      .use(rehypeStringify)
+      .process(input.text)
+      .then(async (vfile: any) => {
+        if (!vfile.data.ping.length) {
+          return;
+        }
+
+        const users = await this.userRepo.find({
+          // you can mention up to 10 people per comment
+          username: In(vfile.data.ping.slice(0, 10)),
+        });
+
+        if (users.length) {
+          await this.questionCommentNotificationRepo
+            .createQueryBuilder()
+            .insert()
+            .values(
+              users.map(u => ({
+                commentId: comment.id,
+                questionId: input.questionId,
+                userToNotifyId: u.id,
+                type: "mention" as "mention",
+              }))
+            )
+            .execute();
+        }
+      });
 
     return {
       comment,
